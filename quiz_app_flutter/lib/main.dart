18c4036ccc74ce9bed373038717abbe8de113a8e
M-Bilal-Jutt <jutt.muhammadbilal@gmail.com>
2021-04-22 03:17:57 +0500

    Quiz App Flutter

--- quiz_app_flutter/lib/main.dart
+++ quiz_app_flutter/lib/main.dart
@@ -1,113 +1,344 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:quiz_app_flutter/quiz.dart';
+import 'dart:math';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+String select;
+int index = 0;
+int correct = 0;
+var questions = new List(10);
+var category = new List(10);
+var correctAnswer = new List(10);
+var allAnswers = new List(10);
+var difficulty = new List(10);
+var type = new List(10);
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => SplashScreen(),
+      '/second': (context) => Home(),
+      '/third': (context) => HomePage(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    Widget example2 = SplashScreenView(
+      home: Home(),
+      duration: 5000,
+      imageSize: 400,
+      imageSrc: "Images/myprofile.png",
+      text: "Muhammad Bilal\nFA17-BCS-051",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: TextStyle(
+        fontSize: 40.0,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      colors: [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.yellow,
+    );
+    return MaterialApp(
+      title: 'Splash screen Demo',
+      home: example2,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          toolbarHeight: 100.0,
+          title: Text(
+            'Quiz App',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 40.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          backgroundColor: Colors.yellow,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(50),
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(60.0),
+                side: BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  select = "boolean";
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                      builder: (context) => new MyApp(),
+                    ),
+                  );
+                });
+              },
+              color: Colors.white,
+              height: 80.0,
+              textColor: Colors.black,
+              child: Text(
+                'True/False',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 37.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              child: Divider(
+                color: Colors.black,
+              ),
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(60.0),
+                side: BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              onPressed: () {
+                select = 'multiple';
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                    builder: (context) => new MyApp(),
+                  ),
+                );
+              },
+              color: Colors.white,
+              height: 80.0,
+              textColor: Colors.black,
+              child: Text(
+                'Multiple Choice',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 37.0,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.white),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Quiz quiz;
+  List<Results> results;
+  Color c;
+  Uri url = Uri.parse("http://opentdb.com/api.php?amount=20&&type=$select");
+  Random random = Random();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> fetchQuestions() async {
+    var res = await http.get(url);
+    var decRes = jsonDecode(res.body);
+    print(decRes);
+    c = Colors.black;
+    quiz = Quiz.fromJson(decRes);
+    results = quiz.results;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 100.0,
+        title: Text(
+          'Quiz App,$correct,$index',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 40.0,
+          ),
+        ),
+        backgroundColor: Colors.yellow,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.push(
+              context,
+              new MaterialPageRoute(
+                builder: (context) => new SplashScreen(),
+              ),
+            );
+          },
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: fetchQuestions,
+        child: FutureBuilder(
+            future: fetchQuestions(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Text('Press button to start.');
+                case ConnectionState.active:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasError) return errorData(snapshot);
+                  for (int i = 1; i < 10; i++) {
+                    questions[i] = results[i].question;
+                    category[i] = results[i].category;
+                    difficulty[i] = results[i].difficulty;
+                    type[i] = results[i].type;
+                    allAnswers[i] = results[i].allAnswers;
+                    correctAnswer[i] = results[i].correctAnswer;
+                  }
+                  return questionList();
+              }
+              return null;
+            }),
+      ),
+    );
+  }
+
+  Column errorData(AsyncSnapshot snapshot) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Error: ${snapshot.error}',
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        RaisedButton(
+          child: Text("Try Again"),
+          onPressed: () {
+            fetchQuestions();
+            setState(() {});
+          },
+        ),
+      ],
+    );
+  }
+
+  Center questionList() {
+    ca = results[index].correctAnswer;
+    return Center(
+      child: ExpansionTile(
+        title: Text(
+          results[index].question,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+        ),
+        children: results[index].allAnswers.map((m) {
+          return FlatButton(
+              onPressed: () {
+                if (m == ca && index < 10) {
+                  setState(() {
+                    index = index + 1;
+                    correct = correct + 1;
+                  });
+                } else if (m != ca && index < 10) {
+                  setState(() {
+                    index = index + 1;
+                  });
+                } else if (index >= 10) {
+                  setState(() {
+                    AlertDialog alert = AlertDialog(
+                      title: Text('Resuly'),
+                      content: Text('$correct/10'),
+                      actions: [
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Ok'),
+                        ),
+                      ],
+                    );
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext) {
+                        return alert;
+                      },
+                    );
+                  });
+                }
+              },
+              child: Text(
+                m,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ));
+        }).toList(),
+      ),
+    );
+  }
+}
+
+String ca;
