086a5bdfb6a43820f3c589da6087e18a7283f8c0
M-Bilal-Jutt <jutt.muhammadbilal@gmail.com>
2021-05-08 12:14:06 +0500

    Calculator App in flutter

---
+++ calculator/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => SplashScreen(),
+      '/second': (context) => Home(),
+    },
+  ));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Widget example2 = SplashScreenView(
+      home: Home(),
+      duration: 5000,
+      imageSize: 400,
+      imageSrc: "Images/myprofile.png",
+      text: "Muhammad Bilal\nFA17-BCS-051",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: TextStyle(
+        fontSize: 40.0,
+      ),
+      colors: [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.black,
+    );
+    return MaterialApp(
+      title: 'Splash screen Demo',
+      home: example2,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: MaterialApp(
+            home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 250.0,
+        title: Text(
+          'Quiz App',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 40.0,
+          ),
+        ),
+        backgroundColor: Colors.yellow,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(80),
+          ),
+        ),
+      ),
+      body: Container(),
+    )));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
